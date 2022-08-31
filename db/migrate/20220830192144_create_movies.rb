a1e439640b68ca3dec8f6935f0031aeb501fd6c1
George <gtduncan@usc.edu>
2022-08-31 15:34:13 -0400

    seeded real movies

--- db/migrate/20220830192144_create_movies.rb
+++ db/migrate/20220830192144_create_movies.rb
@@ -2,11 +2,6 @@ class CreateMovies < ActiveRecord::Migration[7.0]
   def change
     create_table :movies do |t|
       t.string :name
-      t.text :description
-      t.string :rating
-      t.integer :runtime
-      t.string :genre
-      t.datetime :release_date
 
       t.timestamps
     end
