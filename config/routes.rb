e55c2e2b69b749669d5d3af3810d72f7f37bcf3d
George <gtduncan@usc.edu>
2022-08-30 20:10:58 -0400

    new methods and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get "/movies", to: "movies#index"
   get "/theaters", to: "theaters#index"
   get "/movies/:id", to: "movies#show"
+  get "/tickets", to: "tickets#index"
+  patch "/tickets/:id/update", to: "tickets#update"
 
   # Defines the root path route ("/")
   # root "articles#index"
--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_192311) do
   create_table "tickets", force: :cascade do |t|
     t.float "price"
     t.integer "showtime_id"
+    t.boolean "available", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
