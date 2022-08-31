c27121858cde1227f6c2452e51a33aa633d4a130
Ash Dan <Arvolley01@gmail.com>
2022-08-31 14:42:42 -0400

    done for now i guess

--- client/src/Components/MoviePage.js
+++ client/src/Components/MoviePage.js
@@ -3,24 +3,13 @@ import { useState, useEffect } from "react"
 
 const MoviePage = () => {
 
-    const [posters, setPosters] = useState([])
     const [movies, setMovies] = useState([])
 
     useEffect(() => {
-        let newPosters = []
-        const getMovieDetails = async (name) => {
-            let req = await fetch(`http://www.omdbapi.com/?t=${name}&apikey=7ebab2fe`)
-            let res = await req.json()
-            newPosters.push(res.Poster)
-        }
         const getBackend = async () => {
             let req = await fetch(`http://localhost:3000/movies`)
             let res = await req.json()
             setMovies(res)
-            res.forEach((element) => {
-                getMovieDetails(element.name)
-            });
-            setPosters(newPosters)
         }
         getBackend()
     }, [])
@@ -28,11 +17,11 @@ const MoviePage = () => {
 
     return (
         <div>
-            {console.log(posters[0])}
             {movies.map((movie, index) => {
                 return (
                     <div key = { index }>
-                        <img src={posters[index]} alt="movie poster" />
+                        {//<img src={posters[index]} alt="movie poster" />
+                        }
                     </div>
                 )
             })}
