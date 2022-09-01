9f0ea38fec5daf029638ec6f7c66eb4d0426952b
riley soloner <rileysoloner@gmail.com>
2022-09-01 09:33:08 -0400

    added double pipes to some navbar route names

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -4,8 +4,8 @@ import {Link} from 'react-router-dom'
 const NavBar = () => {
     return (
         <div>
-            <Link to="/">Home</Link>
-            <Link to="/movie">Movies</Link>
+            <Link to="/"> Home ||</Link>
+            <Link to="/movie"> Movies ||</Link>
             <Link to="/theater">Theaters</Link>
         </div>
     )
