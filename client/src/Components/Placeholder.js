f0dc6ab12a0e5e9b74aa97fd59b5d5ad0ed8544f
riley soloner <rileysoloner@gmail.com>
2022-09-01 12:01:48 -0400

    added ARG cinemas logo and tagline to homepage/placeholder page

--- client/src/Components/Placeholder.js
+++ client/src/Components/Placeholder.js
@@ -2,7 +2,8 @@
 const PlaceHolder = (props) => {
     return (
         <div className="homePage">
-            <p>Hi! this is a placeholder at {props.from}!</p>
+            <h1 className="argLogo" >A.R.G. Cinemas</h1>
+        <h1 className="argTagline" >Seen any good movies lately?</h1>
         </div>
     )
 }
